f5505b615f5defa002368ced5dbbd0c8ced3e31c
Jameka Echols <jameka.echols@gmail.com>
2020-04-25 16:43:48 -0500

    19 done finally. fuck that was hard as shit

---
+++ top_2009_agent.sql
@@ -0,0 +1,31 @@
+--Which sales agent made the most in sales in 2009? 
+
+-- this works but I don't like it, feel like I can do something else
+--select Top 1 MAX(Customer_Total_Sales) as topSell, Top_Emp_Sale.FullName
+--from (select e.EmployeeId as Emp, CONCAT(e.FirstName, ' ', e.LastName) as FullName, SUM(i.Total) as Customer_Total_Sales
+--	from Invoice i
+--	full join Customer c on c.CustomerId = i.CustomerId
+--	full join Employee e on e.EmployeeId = c.SupportRepId
+--	where i.InvoiceDate like '%2009%'
+--	group by e.EmployeeId, e.FirstName,e.LastName) as Top_Emp_Sale
+--group by Top_Emp_Sale.FullName
+--order by topSell desc
+
+
+--select MAX(Customer_Total_Sales) as topSell,  e.EmployeeId, e.FirstName, e.LastName
+--from (select e.EmployeeId as Emp, SUM(i.Total) as Customer_Total_Sales
+--	from Invoice i
+--	full join Customer c on c.CustomerId = i.CustomerId
+--	full join Employee e on e.EmployeeId = c.SupportRepId
+--	where i.InvoiceDate like '%2009%'
+--	group by e.EmployeeId, e.FirstName,e.LastName) as Top_Emp_Sale 
+--join Employee e on e.EmployeeId = Top_Emp_Sale.Emp
+--group by e.EmployeeId,e.FirstName, e.LastName
+
+select MAX(Customer_Total_Sales)
+from (select e.EmployeeId as Emp, SUM(i.Total) as Customer_Total_Sales
+	from Invoice i
+	full join Customer c on c.CustomerId = i.CustomerId
+	full join Employee e on e.EmployeeId = c.SupportRepId
+	where i.InvoiceDate like '%2009%'
+	group by e.EmployeeId, e.FirstName,e.LastName) as Top_Emp_Sale 
